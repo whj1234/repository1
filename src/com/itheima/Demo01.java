9e7de6bac4b5d8e68c7928371b026273ee3115a0
whj <18817326950@163.com>
2018-12-17 20:36:40 +0800

    提交

---
+++ src/com/itheima/Demo01.java
@@ -0,0 +1,10 @@
+package com.itheima;
+
+public class Demo01 {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+    public void test(){
+        System.out.println("change");
+    }
+}
