498b7558f532fe0af0bc0df3a41c5feeaf24a840
whj <18817326950@163.com>
2018-12-17 20:30:34 +0800

    修改项目

--- src/com/itheima/Demo01.java
+++ src/com/itheima/Demo01.java
@@ -4,4 +4,7 @@ public class Demo01 {
     public static void main(String[] args) {
         System.out.println("hello git");
     }
+    public void test(){
+        System.out.println("change");
+    }
 }
